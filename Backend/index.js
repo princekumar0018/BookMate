b7e00f6bc02007b7469883172bff51a59ccc5257
Prince Kumar <you@example.com>
2025-04-04 14:35:52 +0530

    Cors policy

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,11 @@
 const express=require('express');
-
 const app=express();
+app.use(cors());
+
+app.use('/',(req,res)=>{
+    res.send("Site Is working");
+});
+
 const port=process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
